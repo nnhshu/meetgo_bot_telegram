07f36c1bb8b143c97c607eaa6a15b16533b80c80
nnhshu <nnhshu97@gmail.com>
2024-07-13 14:36:46 +0700

    up

---
+++ src/app/component/vibration.js
@@ -0,0 +1,28 @@
+"use client";
+import React, { useEffect } from 'react';
+
+const VibrationComponent = () => {
+  useEffect(() => {
+    const handleTouch = () => {
+      if (navigator.vibrate) {
+        navigator.vibrate(200); // Rung 200ms
+      }
+    };
+
+    // Thêm sự kiện chạm vào màn hình
+    window.addEventListener('touchstart', handleTouch);
+
+    // Cleanup sự kiện khi component unmount
+    return () => {
+      window.removeEventListener('touchstart', handleTouch);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Chạm vào màn hình để làm rung điện thoại</h1>
+    </div>
+  );
+};
+
+export default VibrationComponent;
