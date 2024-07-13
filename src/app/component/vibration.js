570ceab96c4df4fa7135c41f0bcfcd43de2bf0ca
nnhshu <nnhshu97@gmail.com>
2024-07-13 15:15:37 +0700

    up

--- src/app/component/vibration.js
+++ src/app/component/vibration.js
@@ -1,28 +1,36 @@
 "use client";
 import React, { useEffect } from 'react';
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
 
 const VibrationComponent = () => {
-  useEffect(() => {
-    const handleTouch = () => {
-      if (window.navigator.vibrate) {
-        window.navigator.vibrate(200); // Rung 200ms
-      }
+
+    const hapticsVibrate = async () => {
+        console.log(12)
+        await Haptics.vibrate();
     };
 
-    // Thêm sự kiện chạm vào màn hình
-    window.addEventListener('touchstart', handleTouch);
+    useEffect(() => {
+        const handleTouch = () => {
+            if(window.navigator.userAgentData.mobile){
+                window.navigator.vibrate(100);
+            }
+        };
 
-    // Cleanup sự kiện khi component unmount
-    return () => {
-      window.removeEventListener('touchstart', handleTouch);
-    };
-  }, []);
+        // Thêm sự kiện chạm vào màn hình
+        window.addEventListener('touchstart', handleTouch);
 
-  return (
-    <div>
-      <h1>Chạm vào màn hình để làm rung điện thoại</h1>
-    </div>
-  );
+        // Cleanup sự kiện khi component unmount
+        return () => {
+            window.removeEventListener('touchstart', handleTouch);
+        };
+    }, []);
+
+    return (
+        <div>
+            <h1>Chạm vào màn hình để làm rung điện thoại</h1>
+            <button onClick={() => hapticsVibrate()}>check vibrate</button>
+        </div>
+    );
 };
 
 export default VibrationComponent;
